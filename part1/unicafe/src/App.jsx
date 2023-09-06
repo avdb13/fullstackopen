8bc6f2e97c8dcaa1ea54e744175348747f27cee9
mikoto <avdb@keemail.me>
2023-09-06 21:09:49 +0000

    exercise 1.2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,17 +6,9 @@ const Header = (props) => {
 
 const Content = (props) => {
   return (
-    <>
-      <p>
-        {props.part[0]} {props.exercises[0]}
-      </p>
-      <p>
-        {props.part[1]} {props.exercises[1]}
-      </p>
-      <p>
-        {props.part[2]} {props.exercises[3]}
-      </p>
-    </>
+    <p>
+      {props.part} {props.exercises}
+    </p>
   )
       
 }
@@ -41,7 +33,9 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part={[part1, part2, part3]} exercises={[exercises1, exercises2, exercises3]} />
+      <Content part={part1} exercises={exercises1} />
+      <Content part={part2} exercises={exercises2} />
+      <Content part={part3} exercises={exercises3} />
       <Total exercises={[exercises1, exercises2, exercises3]} />
     </div>
   )
