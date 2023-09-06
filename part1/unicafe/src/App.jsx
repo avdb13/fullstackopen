dd0045e0c469b06143dbb3dd17c8096150451773
mikoto <avdb@keemail.me>
2023-09-06 21:32:55 +0000

    exercise 1.3 (see observation)

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -7,15 +7,15 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <p>
-      {props.part} {props.exercises}
+      {props.part.name} {props.part.exercises}
     </p>
   )
       
 }
 const Total = (props) => {
-  const total = props.exercises.reduce(function add(acc, next) {
-    acc + next
-  }, 0)
+  // observation: `(acc, next) => { acc + next }` and `function(acc, next) { acc + next }`
+  // don't behave like expected here
+  const total = props.exercises.reduce((acc, next) => acc + next, 0)
 
   return (
     <p>Number of exercises {total}</p>
@@ -23,20 +23,28 @@ const Total = (props) => {
 }
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+
+  const exercises = [part1, part2, part3].map(part => part.exercises);
 
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
-      <Total exercises={[exercises1, exercises2, exercises3]} />
+      <Content part={part1} />
+      <Content part={part2} />
+      <Content part={part3} />
+      <Total exercises={exercises} />
     </div>
   )
 }
