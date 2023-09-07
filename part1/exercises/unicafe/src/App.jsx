e3b88a2f826753a3cb4cdffdaccba7636b07fb69
mikoto <avdb@keemail.me>
2023-09-07 16:54:46 +0000

    exercise 1.7

--- part1/exercises/unicafe/src/App.jsx
+++ part1/exercises/unicafe/src/App.jsx
@@ -2,13 +2,34 @@ import { useState } from 'react'
 
 const Header = ({ title }) => <h1>{title}</h1>
 
-const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>
+const Button = ({ text, handleClick }) => <button onClick={handleClick}>{text}</button>
 
-const Statistics = ({ texts, feedbacks }) => {
-  const zipped = feedbacks.map((feedback, i) => [feedback, texts[i]])
-  const mapped = zipped.map(content => <p>{content[0]} {content[1]}</p>)
+const Statistics = ({ texts, feedbacks, average }) => {
+  const NoFeedback = feedbacks.reduce((acc, next) => acc && (next === 0), true)
 
-  return <div>{mapped}</div>
+  // note: this solution is not idiomatic as clicking [good, bad, neutral] will trigger this condition
+  // if (average === 0) {
+  //     return <p>no statistics available yet!</p>
+  // } 
+
+  if (NoFeedback) {
+      return <p>no statistics available yet!</p>
+  } 
+
+  const zipped = feedbacks.map((feedback, i) => [texts[i], feedback])
+  const mapped = zipped.map(([text, feedback]) => <p>{text} {feedback}</p>)
+
+  const positive = feedbacks[0];
+  const totalFeedback = feedbacks.reduce((acc, next) => acc+=next, 0)
+
+  // it is generally disadvised to use array indices without describing their value
+  return (
+    <>
+      <div>{mapped}</div>
+      <p>average {average / totalFeedback}</p>
+      <p>positive {positive / totalFeedback} %</p>
+    </>
+  )
 }
 
 const App = () => {
@@ -16,16 +37,34 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [average, setAverage] = useState(0)
+
+  const handleGood = () => {
+    const updated = good+1
+    setGood(updated)
+    setAverage(average+1)
+  }
+  const handleNeutral = () => {
+    const updated = neutral+1
+    setNeutral(updated)
+    setAverage(average+0)
+  }
+  const handleBad = () => {
+    const updated = bad+1
+    setBad(updated)
+    setAverage(average-1)
+  }
 
   return (
     <div>
      <Header title='give feedback' />
-     <Button handleClick={() => setGood(good+1)} text='good' />
-     <Button handleClick={() => setNeutral(neutral+1)} text='neutral' />
-     <Button handleClick={() => setBad(bad+1)} text='bad' />
+
+     <Button handleClick={handleGood} text='good' />
+     <Button handleClick={handleNeutral} text='neutral' />
+     <Button handleClick={handleBad} text='bad' />
 
      <Header title='statistics' />
-     <Statistics texts={['good', 'neutral', 'bad']} feedbacks={[good, neutral, bad]} />
+     <Statistics texts={['good', 'neutral', 'bad']} feedbacks={[good, neutral, bad]} average={average} />
     </div>
   )
 }
