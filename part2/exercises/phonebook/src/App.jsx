fafc067f89ae55ef952c73f0191c8bbcd698dbe2
mikoto <avdb@keemail.me>
2023-09-08 16:35:22 +0000

    exercise 2.9

--- part2/exercises/phonebook/src/App.jsx
+++ part2/exercises/phonebook/src/App.jsx
@@ -2,23 +2,25 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    {
-      name: 'Arto Hellas',
-      number: '040-1234567'
-    }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [newFilter, setNewFilter] = useState('')
 
   const handleName = (event) => setNewName(event.target.value)
   const handleNumber = (event) => setNewNumber(event.target.value)
+  const handleFilter = (event) => setNewFilter(event.target.value)
 
   const addPerson = (event) => {
     event.preventDefault()
     if (persons.find(person => person.name == newName)) {
       alert(`${newName} is already added to the phonebook`)
     } else {
-      setPersons([...persons, { name: newName, number: newNumber}])
+      setPersons([...persons, { name: newName, number: newNumber, id: persons.length+1}])
     }
     
     // note: for ergonomics we also remove the name when it's a duplicate
@@ -26,9 +28,17 @@ const App = () => {
     setNewNumber('')
   }
 
+  const filtered = newFilter ? persons.filter(person =>
+    person.name.toLowerCase().startsWith(newFilter.toLowerCase())
+  ) : persons
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input onChange={handleFilter} />
+      </div>
+      <h2>Add new contact</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleName} />
@@ -42,7 +52,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(person => <li key={person.name}>{person.name} {person.number}</li>)}
+        {filtered.map(person => <li key={person.id}>{person.name} {person.number}</li>)}
       </ul>
     </div>
   )
