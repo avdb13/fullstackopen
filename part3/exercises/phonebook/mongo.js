e56a7be44ae5d87acd57c5432b8818de7ec33022
mikoto <avdb@keemail.me>
2023-09-11 20:38:50 +0000

    exercise 3.12

--- part3/exercises/phonebook/mongo.js
+++ part3/exercises/phonebook/mongo.js
@@ -4,11 +4,6 @@ const mongoose = require("mongoose");
 
 const url = "mongodb://localhost:27017/phonebook?retryWrites=true&w=majority";
 
-if (process.argv.length < 4) {
-  console.log("USAGE: node mongo.js [contact] [phone number]");
-  process.exit(1);
-}
-
 mongoose.set("strictQuery", false);
 mongoose.connect(url);
 
@@ -19,12 +14,26 @@ const contactSchema = new mongoose.Schema({
 
 const Contact = mongoose.model("Contact", contactSchema);
 
-const contact = new Contact({
-  name: process.argv[2],
-  number: process.argv[3],
-});
+if (process.argv.length === 2) {
+  Contact.find({}).then((result) => {
+    result.forEach((contact) => {
+      console.log(contact);
+    });
 
-contact.save().then(() => {
-  console.log(`created new contact: [${contact.name} ${contact.number}]`);
-  mongoose.connection.close();
-});
+    mongoose.connection.close();
+  });
+} else if (process.argv.length === 4) {
+  const contact = new Contact({
+    name: process.argv[2],
+    number: process.argv[3],
+  });
+
+  contact.save().then(() => {
+    console.log(`created new contact: [${contact.name} ${contact.number}]`);
+
+    mongoose.connection.close();
+  });
+} else {
+  console.log("USAGE: node mongo.js [contact] [phone number]");
+  process.exit(1);
+}
