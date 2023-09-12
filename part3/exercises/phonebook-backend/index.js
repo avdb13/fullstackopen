f633e1d2f38ccdb64d9fa507906f52f3ffca38f7
mikoto <avdb@keemail.me>
2023-09-12 21:26:27 +0000

    exercise 3.21

--- part3/exercises/phonebook-backend/index.js
+++ part3/exercises/phonebook-backend/index.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const cors = require("cors");
-const mongoose = require("mongoose");
 const morgan = require("morgan");
+const Contact = require("./models/contacts");
 
-const url = "mongodb://localhost:27017/contacts";
 const app = express();
 
 app.use(cors());
@@ -18,29 +17,14 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body"),
 );
 
-// initialization
-mongoose.connect(url).catch((e) => console.log(`something went wrong: ${e}}`));
-
-const contactSchema = new mongoose.Schema({
-  name: String,
-  number: String,
-});
-
-contactSchema.set("toJSON", {
-  transform: (document, retObject) => {
-    retObject.id = retObject._id.toString();
-    delete retObject._id;
-    delete retObject.__v;
-  },
-});
-
-const Contact = mongoose.model("Contact", contactSchema);
-
 const errorHandler = (err, req, resp, next) => {
-  console.log(err.message);
+  console.log(err);
 
   if (err.name === "TypeError") {
     return resp.status(400).json({ error: "malformed body" });
+  } else if (err.name === "ValidationError") {
+    console.log("properly handled error");
+    return resp.status(400).json({ error: err.message });
   }
 
   next(err);
@@ -68,35 +52,33 @@ app.get("/api/contacts/:id", (req, resp, next) => {
 app.post("/api/contacts", (req, resp, next) => {
   const body = req.body;
 
-  if (!body.name || !body.number) {
-    // how to pass this to the error handler?
-    resp.status(400).json({ error: "body missing or malformed" });
-  }
-
   const contact = new Contact({
     name: body.name,
     number: body.number,
   });
 
-  contact.save().then((newContact) => {
-    resp.json(newContact);
-  });
+  contact
+    .save()
+    .then((newContact) => {
+      resp.json(newContact);
+    })
+    .catch((e) => next(e));
 });
 
 app.put("/api/contacts/:id", (req, resp, next) => {
   const id = req.params.id;
   const body = req.body;
 
-  if (!body.name || !body.number) {
-    return resp.status(400).json({ error: "body missing or malformed" });
-  }
-
   const contact = {
     name: body.name,
     number: body.number,
   };
 
-  Contact.findByIdAndUpdate(id, contact, { new: true })
+  Contact.findByIdAndUpdate(id, contact, {
+    new: true,
+    runValidators: true,
+    context: "query",
+  })
     .then((newContact) => {
       resp.json(newContact);
     })
@@ -113,6 +95,8 @@ app.delete("/api/contacts/:id", (req, resp, next) => {
     })
     .catch((e) => next(e));
 });
+
+app.use(errorHandler);
 
 const PORT = 3000;
 
---
+++ part3/exercises/phonebook-backend/models/contacts.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .catch((e) => console.log(`something went wrong: ${e}}`));
+
+const contactSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minLength: 3,
+    required: true,
+  },
+  number: {
+    type: String,
+    minLength: 8,
+    validate: {
+      validator: (s) => /\d{2,3}-\d{6,}/.test(s),
+      message: (props) => `${props.value} is not a valid phone number`,
+    },
+    required: [true, "Phone number required"],
+  },
+});
+
+contactSchema.set("toJSON", {
+  transform: (document, retObject) => {
+    retObject.id = retObject._id.toString();
+    delete retObject._id;
+    delete retObject.__v;
+  },
+});
+
+module.exports = mongoose.model("Contact", contactSchema);
