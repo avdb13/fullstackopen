d516cf3c52de905234b39a98be83285aba320903
mikoto <avdb@keemail.me>
2023-09-13 13:32:53 +0000

    exercise 4.1

---
+++ part4/exercises/blog/models/blog.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+});
+
+module.exports = mongoose.model("Blog", blogSchema);
