c1e5c554602d894cae0913d6b91330edd078e7f5
mikoto <avdb@keemail.me>
2023-09-18 19:25:24 +0000

    exercise 5.15

--- part5/exercises/blog-frontend/tests/components/Blog.test.js
+++ part5/exercises/blog-frontend/tests/components/Blog.test.js
@@ -4,7 +4,7 @@ import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Blog from '../../src/components/Blog'
 
-describe('<Blog />', () => {
+describe('<Blog /> : displaying information', () => {
   let container
 
   beforeEach(() => {
@@ -39,5 +39,35 @@ describe('<Blog />', () => {
 
     expect(urlEl).toBeDefined()
     expect(likesEl).toBeDefined()
+  })
+})
+
+describe('<Blog /> : calling even handlers', () => {
+  let container
+  let mockHandler
+
+  beforeEach(() => {
+    const blog = {
+      title: 'testing article',
+      url: 'https://testing-library.com',
+      author: 'Kent C. Dodds',
+      likes: 12,
+      user: { name: 'Testing Library' }
+    }
+
+    mockHandler = jest.fn()
+
+    container = render(<Blog blog={blog} addLike={mockHandler} />).container
+  })
+
+  test('if like is clicked twice, the event handler is called twice', async () => {
+    const user = userEvent.setup()
+    const showButton = screen.getByText('show')
+    await user.click(showButton)
+    const likeButton = screen.getByText('like')
+    await user.click(likeButton)
+    await user.click(likeButton)
+
+    expect(mockHandler.mock.calls).toHaveLength(2)
   })
 })
