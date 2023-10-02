4245feebb39863c8474d79cdecad5f245bf8c0fc
mikoto <avdb@keemail.me>
2023-10-02 18:56:29 +0000

    exercise 9.5

--- part9/exercises/express/index.ts
+++ part9/exercises/express/index.ts
@@ -1,9 +1,21 @@
 import express, { Request, Response } from "express";
+import { calculator } from "./bmiCalculator";
 
 const app = express()
 
 app.get('/hello', (_req: Request, resp: Response) => {
   resp.send('Hello Full Stack!')
+})
+
+app.get('/bmi', express.json(), (req: Request, resp: Response) => {
+  const weight = req.query.weight
+  const height = req.query.height
+  if (!isNaN(Number(weight)) && !isNaN(Number(height))) {
+    const result = calculator(Number(weight), Number(height))
+    resp.json({ weight: Number(weight), height: Number(height), result })
+  } else {
+    resp.send({ error: 'weight and height must be numbers'})
+  }
 })
 
 const PORT = 3000
