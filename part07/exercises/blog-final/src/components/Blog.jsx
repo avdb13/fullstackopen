bcff7d91c920448dcd003007f9dd0b425ea8f4eb
mikoto <avdb@keemail.me>
2023-10-19 20:34:17 +0000

    blog CSS

--- part07/exercises/blog-final/src/components/Blog.jsx
+++ part07/exercises/blog-final/src/components/Blog.jsx
@@ -1,10 +1,7 @@
 import { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { commentBlog } from '../reducers/blogReducer'
-import dayjs from 'dayjs'
-import relativeTime from 'dayjs/plugin/relativeTime'
-
-dayjs.extend(relativeTime)
+import Comment from '../components/Comment'
 
 const Blog = ({ blog, removeBlog, addLike }) => {
   const dispatch = useDispatch()
@@ -12,16 +9,6 @@ const Blog = ({ blog, removeBlog, addLike }) => {
   const [showAll, setShowAll] = useState(false)
 
   if (!blog) return <div>this blog does not exist anymore!</div>
-
-  // const blogStyle = {
-  //   listStyle: 'none',
-  //   paddingTop: 10,
-  //   paddingLeft: 10,
-  //   border: 'solid',
-  //   borderWidth: 2,
-  //   borderRadius: 5,
-  //   marginBottom: 5,
-  // }
 
   const handleRemove = () => {
     if (window.confirm(`remove ${blog.title} by ${blog.author}?`)) {
@@ -38,22 +25,21 @@ const Blog = ({ blog, removeBlog, addLike }) => {
     dispatch(commentBlog(blog, comment))
   }
 
-  const blogStyle = 'relative mx-4 my-2 p-2 max-w-md flex flex-shrink place-items-center flex-wrap justify-between bg-white shadow-sm ring-2 ring-purple-300 rounded'
+  const blogStyle = 'relative mx-4 my-2 p-2 max-w-md bg-white shadow-sm ring-2 ring-purple-300 rounded'
+  const flexStyle = 'flex flex-shrink place-items-center flex-wrap justify-between'
   const gradient = 'absolute mx-4 my-2 blur -inset-0 max-w-md transition absolute bg-gradient-to-r from-indigo-400 to-purple-400 opacity-10 group-hover:opacity-50'
   const buttonStyle = 'shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none font-bold rounded text-white py-1 px-4 m-2'
   const likeStyle = 'flex justify-center m-1 rounded-full'
 
 
   const compactView = () => (
-    <div>
-      <li key={blog.id} className="relative group">
-        <div className={gradient}></div>
-        <div className={blogStyle}>
-          <a className='basis-1/2 text-xl font-semibold text-gray-600' href={blog.url}>{blog.title}</a>
-          <button className={buttonStyle} onClick={() => setShowAll(true)}>show</button>
-          <p className='basis-1/2 text-xs'>by {blog.author}</p>
-        </div>
-      </li>
+    <div key={blog.id} className="relative group">
+      <div className={gradient}></div>
+      <div className={blogStyle + ' ' + flexStyle}>
+        <a className='basis-1/2 text-xl font-semibold text-gray-600' href={blog.url}>{blog.title}</a>
+        <button className={buttonStyle} onClick={() => setShowAll(true)}>show</button>
+        <p className='basis-1/2 text-xs'>by {blog.author}</p>
+      </div>
     </div>
   )
 
@@ -61,35 +47,41 @@ const Blog = ({ blog, removeBlog, addLike }) => {
     <li key={blog.id} className="relative group">
       <div className={gradient}></div>
       <div className={blogStyle}>
-        <h2 className='basis-1/2 text-xl font-semibold text-gray-600'>
-          {blog.title}
-        </h2>
-        <button className={buttonStyle} onClick={() => setShowAll(false)}>hide</button>
-        <h2 className='basis-1/2 text-xs'>
+        <div className={flexStyle}>
+          <h2 className='basis-1/2 text-xl font-semibold text-gray-600'>
+            {blog.title}
+          </h2>
+          <button className={buttonStyle} onClick={() => setShowAll(false)}>hide</button>
+          <h2 className='basis-1/2 text-xs'>
            by {blog.author}{' '}
-        </h2>
-        <div className='pt-4'>
-          <a className='text-violet-900 hover:text-violet-700 font-thin' href={blog.url}>{blog.url}</a>
-          <p className='basis-1/2 text-xs'>
+          </h2>
+        </div>
+        <div className={flexStyle}>
+          <div className='pt-4'>
+            <a className='text-violet-900 hover:text-violet-700 font-thin' href={blog.url}>{blog.url}</a>
+            <p className='basis-1/2 text-xs'>
             added by {blog.user.name}{' '}
-            {user ? (user.username === blog.user.username ? (
-              <button className={buttonStyle} onClick={() => handleRemove(blog)}>remove</button>
-            ) : null) : null}
-          </p>
-          <div className='flex flex-initial items-center py-2'>
-            <p>
-              {blog.likes ? blog.likes : 0} likes{' '}
+              {user ? (user.username === blog.user.username ? (
+                <button className={buttonStyle} onClick={() => handleRemove(blog)}>remove</button>
+              ) : null) : null}
             </p>
-            <button className={buttonStyle} id="like-button" onClick={() => addLike(blog)}>
+            <div className='flex flex-initial items-center py-2'>
+              <p>
+                {blog.likes ? blog.likes : 0} likes{' '}
+              </p>
+              <button className={buttonStyle} id="like-button" onClick={() => addLike(blog)}>
             like
-            </button>
+              </button>
+            </div>
           </div>
+        </div>
+        <div>
           <h3>comments</h3>
-          <ul>
-            {blog.comments.map(comment => <li key={comment.body}>{comment.body} {dayjs(comment.added).fromNow()}</li>)}
+          <ul className='w-full'>
+            {blog.comments.map(comment => <Comment key={comment._id} comment={comment} />)}
           </ul>
-          <form onSubmit={handleAddComment}>
-            <input type='text' name='comment' />
+          <form onSubmit={handleAddComment} className={flexStyle}>
+            <input type='text' name='comment' className="grow py-1 text-center rounded focus:-border-1 bg-slate-100" />
             <button className={buttonStyle} type='submit'>add comment</button>
           </form>
         </div>
---
+++ part07/exercises/blog-final/src/components/Comment.jsx
@@ -0,0 +1,22 @@
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
+import UserSvg from './User'
+
+dayjs.extend(relativeTime)
+
+const Comment = ({ comment }) => (
+  <li className='flex flex-col my-2'>
+    <div className='flex gap-1 grow justify-between m-2 items-center'>
+      <div className='flex gap-1 items-center font-bold'>
+        <UserSvg />
+        <p>anonymous</p>
+      </div>
+      <p className='text-sm text-gray-500'>{dayjs(comment.added).fromNow()}</p>
+    </div>
+    <div>
+    </div>
+    <p>{comment.body}</p>
+  </li>
+)
+
+export default Comment
